2e8672fb831bcbc02bc2c1e9302f94f01ff39f53
Gabriel <51007820+wgaabe@users.noreply.github.com>
2021-05-06 14:51:34 -0300

    Update 3-Limpeza e Otimizacao do Windows.bat

--- Winopt/Winopt/3-Limpeza e Otimizacao do Windows.bat
+++ Winopt/Winopt/3-Limpeza e Otimizacao do Windows.bat
@@ -28,7 +28,10 @@ DISM /Online /Cleanup-Image /RestoreHealth
 @echo off    
 del /s /f /q %windir%\temp\*.*    
 rd /s /q %windir%\temp    
-md %windir%\temp    
+md %windir%\temp
+del /s /f /q %temp%\*.*    
+rd /s /q %temp%    
+md %temp%   
 del /s /f /q %windir%\Prefetch\*.*    
 rd /s /q %windir%\Prefetch    
 md %windir%\Prefetch    
